083f212cb8842e5786ebda0a23adf94c072dd32f
Mr Chen <846825177@qq.com>
2017-09-12 10:06:15 +0800

    分享

---
+++ app/src/main/java/com/chenzeyang/myappdemp/MyService.java
@@ -0,0 +1,54 @@
+package com.chenzeyang.myappdemp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+
+/**
+ * ----------------Dragon be here!------------------
+ * 　   　　┏┓　　　┏┓
+ * 　   　┏┛┻━━━┛┻┓
+ * 　   　┃　　　　　　　┃
+ * 　   　┃　　　━　　　┃
+ * 　   　┃　┳┛　┗┳　┃
+ * 　   　┃　　　　　　　┃
+ * 　   　┃　　　┻　　　┃
+ * 　   　┃　　　　　　　┃
+ * 　   　┗━┓　　　┏━┛
+ * 　   　　　┃　　　┃神兽保佑
+ * 　   　　　┃　　　┃代码无BUG！
+ * 　   　　　┃　　　┗━━━┓
+ * 　   　　　┃　神兽出品　　┣┓
+ * 　   　　　┃　必属精品　　┏┛
+ * 　   　　　┗┓┓┏━┳┓┏┛
+ * 　   　　　　┃┫┫　┃┫┫
+ * 　   　　　　┗┻┛　┗┻┛
+ * ━━━━━━━━神兽出没━━━━━━━━
+ * <p>
+ * Created by Mr Chen on 2017/9/11 0011 下午 5:10.
+ */
+
+public class MyService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
